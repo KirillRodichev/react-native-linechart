58b70bd3049e5bdf9ad389687f547a238e3a85d0
Kirill Rodichev <kirill.rodichev38@gmail.com>
2024-04-15 22:46:04 +0100

    refactoring: lint

--- src/components/LineChartPointer/hooks/useAnimatedPulsingValues.ts
+++ src/components/LineChartPointer/hooks/useAnimatedPulsingValues.ts
@@ -1,29 +1,32 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
 
 import {
-	Easing,
-	useSharedValue,
-	withRepeat,
-	withTiming,
-} from 'react-native-reanimated'
+  Easing,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from 'react-native-reanimated';
 
 const ANIMATION_CONFIG = {
-	duration: 800,
-	easing: Easing.out(Easing.ease),
-}
+  duration: 800,
+  easing: Easing.out(Easing.ease),
+};
 
 interface IProps {
-	scaleCoefficient: number
+  scaleCoefficient: number;
 }
 
 export const useAnimatedPulsingValues = ({ scaleCoefficient }: IProps) => {
-	const scale = useSharedValue(1)
-	const opacity = useSharedValue(1)
+  const scale = useSharedValue(1);
+  const opacity = useSharedValue(1);
 
-	useEffect(() => {
-		scale.value = withRepeat(withTiming(scaleCoefficient, ANIMATION_CONFIG), -1)
-		opacity.value = withRepeat(withTiming(0, ANIMATION_CONFIG), -1)
-	}, [])
+  useEffect(() => {
+    scale.value = withRepeat(
+      withTiming(scaleCoefficient, ANIMATION_CONFIG),
+      -1
+    );
+    opacity.value = withRepeat(withTiming(0, ANIMATION_CONFIG), -1);
+  }, [opacity, scale, scaleCoefficient]);
 
-	return { scale, opacity }
-}
+  return { scale, opacity };
+};
